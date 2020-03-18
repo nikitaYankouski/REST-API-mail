f0d2ae5877bb98175f6108f5bcce72ab3bdef857
skeleton <nikitayankovskij@gmail.com>
2020-03-18 20:50:11 +0100

    delete dependency mail

---
+++ src/test/java/com/hudman/RestApiMail/RestApiMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudman.RestApiMail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
