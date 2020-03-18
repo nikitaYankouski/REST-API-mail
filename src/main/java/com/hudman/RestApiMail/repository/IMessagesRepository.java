f664d8f92596c44d7f3bc7c665e01b2006230b88
skeleton <nikitayankovskij@gmail.com>
2020-03-18 21:24:59 +0100

    add type in cassandraRepository

--- src/main/java/com/hudman/RestApiMail/repository/IMessagesRepository.java
+++ src/main/java/com/hudman/RestApiMail/repository/IMessagesRepository.java
@@ -1,6 +1,9 @@
 package com.hudman.RestApiMail.repository;
 
+import com.hudman.RestApiMail.model.Message;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-public interface IMessagesRepository extends CassandraRepository<> {
+import java.util.UUID;
+
+public interface IMessagesRepository extends CassandraRepository<Message, UUID> {
 }
