e6a8eba6eead64b15b5469ffa8e48903574d2c1a
skeleton <nikitayankovskij@gmail.com>
2020-03-19 17:07:58 +0100

    config cassandra
    add create keyspace

---
+++ src/main/java/com/hudman/RestApiMail/config/CassandraConfig.java
@@ -0,0 +1,52 @@
+package com.hudman.RestApiMail.config;
+
+import com.datastax.driver.core.Cluster;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CassandraCqlClusterFactoryBean;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.DataCenterReplication;
+import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@Configuration
+public class CassandraConfig extends AbstractCassandraConfiguration {
+    public static final String KEYSPACE = "dataBaseMessages";
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE);
+
+        return Arrays.asList(specification);
+    }
+
+    @Override
+    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
+        return Arrays.asList(DropKeyspaceSpecification.dropKeyspace(KEYSPACE));
+    }
+
+    @Override
+    protected String getKeyspaceName() {
+        return KEYSPACE;
+    }
+
+    @Override
+    public String[] getEntityBasePackages() {
+        return new String[]{"com.hudman.RestApiMail.model"};
+    }
+
+}
